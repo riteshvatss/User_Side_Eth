5ff1afc933c6a2705cf56e4a7a9fdca9b3abc60b
riteshvatss <riteshvats0@gmail.com>
2025-06-01 14:24:04 +0530

    first

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiProvider } from 'wagmi'
+import { config } from './config'
+import { WalletOptions } from './Walletoptions'
+import { Account } from './Account'
+
+const queryClient = new QueryClient()
+import { useAccount } from 'wagmi'
+
+//  function Profile() {
+//   const { address } = useAccount()
+//   const { data, error, status } = useEnsName({ address })
+//   if (status === 'pending') return <div>Loading ENS name</div>
+//   if (status === 'error')
+//     return <div>Error fetching ENS name: {error.message}</div>
+//   return <div>ENS name: {data}</div>
+// }
+
+
+function ConnectWallet() {
+  const { isConnected } = useAccount()
+  if (isConnected) return <Account />
+  return <WalletOptions />
+}
+
+function App() {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+           <ConnectWallet></ConnectWallet>
+      </QueryClientProvider>
+    </WagmiProvider>
+  )
+}
+
+export default App;
---
+++ src/config.ts
@@ -0,0 +1,16 @@
+import { createConfig, http } from 'wagmi'
+import { mainnet} from 'wagmi/chains'
+import { injected,metaMask, } from 'wagmi/connectors'
+
+export const config = createConfig({ 
+    chains: [mainnet],
+    connectors: [
+        injected(),
+        metaMask(),
+      
+    ],
+ 
+  transports: { 
+    [mainnet.id]: http(), 
+  }, 
+})
